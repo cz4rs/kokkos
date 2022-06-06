aa80d144ee92b3eb02ea6e8f746e19902e45ee16
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2022-06-06 16:33:00 +0200

    Use templated benchmark

--- core/perf_test/benchmark.cpp
+++ core/perf_test/benchmark.cpp
@@ -4,19 +4,18 @@
 
 #include <PerfTest_ViewCopy.hpp>
 
-static void ViewDeepCopy_RightLeft_Rank8(benchmark::State& state) {
+template <class LayoutA, class LayoutB>
+static void ViewDeepCopy_Rank8(benchmark::State& state) {
   // Perform setup here
-  printf("DeepCopy Performance for LayoutRight to LayoutLeft:\n");
-
+  // ...
   for (auto _ : state) {
     // This code gets timed
-    Test::run_deepcopyview_tests8<Kokkos::LayoutRight, Kokkos::LayoutLeft>(10,
-                                                                           1);
+    Test::run_deepcopyview_tests8<LayoutA, LayoutB>(10, 1);
   }
 }
 
 // Register the function as a benchmark
-BENCHMARK(ViewDeepCopy_RightLeft_Rank8);
+BENCHMARK(ViewDeepCopy_Rank8<Kokkos::LayoutRight, Kokkos::LayoutLeft>);
 
 // Run benchmarks
 // BENCHMARK_MAIN() + Kokkos init / finalize
